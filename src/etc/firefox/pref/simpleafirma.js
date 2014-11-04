907e992a2da81b827a3316f99627444662ce1f24
Alfonso E.M <alfonso@el-magnifico.org>
2014-11-04 14:15:07 +0100

    Revisado el empaquetado y corregidas las dependencias

---
+++ src/etc/firefox/pref/simpleafirma.js
@@ -0,0 +1,4 @@
+pref("network.protocol-handler.app.afirma","/usr/bin/simpleafirma");
+pref("network.protocol-handler.warn-external.afirma",false);
+pref("network.protocol-handler.external.afirma",true);
+
